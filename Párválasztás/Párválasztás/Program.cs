b1bd0cffc6029c57c4c4bbac8d901e66740369e1
Farkas0123 <ukosfarkas@gmail.com>
2023-03-15 21:20:23 +0100

    Elkezdve a gondolat

---
+++ Párválasztás/Párválasztás/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Párválasztás
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine("Szükséges feltételek felmérése, gondolat megszületése.");
+                if (i % 3 == 1)
+                    Console.WriteLine("Szükséges feltételek felmérése, gondolat megszületése..");
+                if (i % 3 == 2)
+                    Console.WriteLine("Szükséges feltételek felmérése, gondolat megszületése...");
+                
+            }
+            
+        }
+    }
+}
